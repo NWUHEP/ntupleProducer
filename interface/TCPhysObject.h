47c520893aacf4f5ab3161f1de7378739d482c6a
naodell <>
2012-08-27 20:30:39 +0000

    adding physics object base object

---
+++ interface/TCPhysObject.h
@@ -0,0 +1,50 @@
+#ifndef _TCPHYSOBJECT_H
+#define	_TCPHYSOBJECT_H
+
+#include "TObject.h"
+#include "TLorentzVector.h"
+#include "TVector2.h"
+#include "TVector3.h"
+#include <map>
+#include <utility>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class TCPhysObject : public TLorentzVector {
+    private:
+        TVector3 _vtx;
+        map<string, float> _IdMap;
+        map<string, float> _IsoMap;
+        int _charge;
+        string _type;
+
+    public:
+        TCPhysObject();
+        TCPhysObject(TLorentzVector p4, int charge);
+        virtual ~TCPhysObject();
+
+        // "get" methods -----------
+
+        float IdMap(string key);
+        float IsoMap(string key);
+        TVector2 P2() const;
+        TVector3 Vtx() const;
+        int Charge() const;  
+        string Type() const;
+        float Dxy(TVector3 *primVtx) const;
+        float Dz(TVector3 *primVtx) const;
+
+        // "set" methods ---------
+        void SetP4(TLorentzVector p4);
+        void SetIdMap(string s, float v);
+        void SetIsoMap(string s, float v);
+        void SetVtx(float vx, float vy, float vz);
+        void SetCharge(int c);  
+        void SetType(string s);
+
+        ClassDef(TCPhysObject, 1);
+};
+
+#endif	/* _TCPHYSOBJECT_H */
---
+++ src/TCPhysObjectLinkDef.h
@@ -0,0 +1,5 @@
+#include "Higgs/ntupleProducer/interface/TCPhysObject.h"
+
+#ifdef __CINT__
+#pragma link C++ class TCPhysObject;
+#endif
