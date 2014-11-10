68e58704c0e99df7cadee722fc59fbe41dcd7a8d
Andrey Pozdnyakov <Andrey.Pozdnyakov@cern.ch>
2014-11-10 13:17:27 -0600

    link def

--- src/TCPhysObjectLinkDef.h
+++ src/TCPhysObjectLinkDef.h
@@ -2,5 +2,6 @@
 
 #ifdef __CINT__
 //#pragma link C++ class std::map<string, float>+;  //already pre-compiled object, don't need a link?
+#pragma link C++ class std::pair<string, vector<string> >+;
 #pragma link C++ class TCPhysObject+;
 #endif
